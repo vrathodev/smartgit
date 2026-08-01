5eed4f9a972b9f3b27a043e5a8b31df04675020d
Vipul Rathod <vrathod.professional@gmail.com>
2026-08-01 12:37:48 +0530

    feat: added global options: verbosity & quite mode

--- src/smartgit/app/cli/main.py
+++ src/smartgit/app/cli/main.py
@@ -30,11 +30,23 @@ def main_callback(
         Optional[Path],
         Option('--config', help='Path to an explicit SmartGit configuration file.'),
     ] = None,
+    verbosity: Annotated[
+        int,
+        Option('-v', count=True, help='Verbosity levels (-v: WARNING, -vv: INFO, -vvv: DEBUG)')
+    ] = 0,
+    quiet: Annotated[
+        bool,
+        Option('--quiet', '-q', help='Quiet mode. Suppresses all output except for errors.')
+    ] = False
 ):
     """
     Initializes per-invocation CLI context
 
     :param ctx:             Typer shared-context for associated CLI invocation
+    :param verbosity:       [Optional] Verbosity level for logging.
+                            -v: WARNING, -vv: INFO, -vvv: DEBUG
+                            Defaults to 0 (ERROR)
+    :param quiet:           [Optional] Quiet mode. Suppresses all logging output except for errors.
     :param config_path:     [Optional] Explicit SmartGit config file path. Defaults to None
     """
     LOGGER.entrance()
@@ -42,6 +54,13 @@ def main_callback(
     if ctx.resilient_parsing:
         LOGGER.debug('Invoked with Resilient Parsing mode ON, skipping...')
         return
+
+    # TODO:
+    #  configSmartLogger() logs already, before quiet mode is identified here,
+    #  resolve multiple loggers maintenance overhead for CLI, Core, MCP, etc.
+    verbosity_level: int = 40 - (10 * min(verbosity, 3)) if not quiet else 0
+    LOGGER.setLevel(verbosity_level)
+    _.setLevel(verbosity_level)
 
     try:
         ctx.ensure_object(dict)
